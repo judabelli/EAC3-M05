63bdc058e9f63bc5816c1892602b66f3c45aa94e
judit <jud.abelli@gmail.com>
2022-11-30 11:19:45 +0100

    commit inicial

---
+++ Gos.java
@@ -0,0 +1,23 @@
+package Mamifers;
+
+public class Gos {
+	String raca;
+	String origen;
+	
+	public void setRaca (String raca){
+		this.raca=raca;
+	}
+	
+	public String getRaca(){
+		return raca;
+	}
+
+	public void setOrigen (String origen){
+		this.origen=origen;
+	}
+	
+	public String getOrigen(){
+		return origen;
+	}
+}
+
